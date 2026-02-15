eee7784df7d73863a82f66aa846e5daff427513b
luanaeringercosta <luanascosta19@gmail.com>
2026-02-15 19:46:31 -0300

    perfil, footer e navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import Inicio from "../Inicio/Inicio";
-import Acoes from "../AcoesDaConnect/AcoesDaConnect";
-import QuemSomos from "../QuemSomos/QuemSomos";
-import Perfil from "../Perfil/Perfil";
+import Inicio from "../Inicio/Inicio.jsx";
+import Acoes from "../AcoesDaConnect/AcoesDaConnect.jsx";
+import QuemSomos from "../QuemSomos/QuemSomos.jsx";
+import Perfil from "../Perfil/Perfil.jsx";
 
 import LogoTrabalho from "../../assets/logo.png";
 import FotoDoPerfil from "../../assets/perfil.jpg";
@@ -15,30 +15,35 @@ function Navbar() {
     return (
         <BrowserRouter>
         
-        <nav className= {styles.navBar}>
+        <nav className= {styles.navbar}>
             <Link to="/">
             <img className={styles.logo} src= {LogoTrabalho} alt="Logo" />
             </Link>
 
-            <ul>
-                <li> <Link to = "/"> </Link>Inicio</li>
-                <li> <Link to = "/acoes"> </Link>Ações da Connect</li>
-                <li> <Link to = "/quemSomos"> </Link>Quem Somos</li>
-            </ul>
+             <ul>
+          <li>
+            <Link to="/"> Início </Link>
+          </li>
+          <li>
+            <Link to="/acoes"> Ações da Connect</Link>
+          </li>
+          <li>
+            <Link to="/quemSomos"> Quem Somos </Link>
+          </li>
+        </ul>
+
              <Link to="/perfil">
             <img className={styles.imgPerfil} src= {FotoDoPerfil} alt="Perfil" />
             </Link>
         </nav>
 
-        <Routes>
-            <Route path ="/" element={<Inicio />}/>
-            <Route path ="/acoes" element= {<Acoes/>}/>
-            <Route path= "/quemSomos" element= {<QuemSomos/>}/>
-            <Route path = "/perfil" element= {<Perfil/>}/>
-
-
-        </Routes>
-        </BrowserRouter>
+         <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/acoes" element={<Acoes />} />
+        <Route path="/quemSomos" element={<QuemSomos />} />
+        <Route path="/perfil" element={<Perfil />} />
+      </Routes>
+    </BrowserRouter>
     );
 }
 
--- src/components/Perfil/Perfil.jsx
+++ src/components/Perfil/Perfil.jsx
@@ -5,25 +5,24 @@ import ImgPerfil from "../../assets/perfil.jpg";
 function Perfil () {
     return(
         <section className={styles.containerTelaPerfil}>
-        <h2>Perfil</h2>
-
         <article>
 
         <img src={ImgPerfil} alt="Foto de Pefil" />
         <div className={styles.cardInfo}>
             <h2>Luana Costa</h2>
-            <h3>voluntaria??</h3>
-            <p></p>
+            <h3>Estudante</h3>
+            <p className= {styles.paragrafoSobreMim}>
+                Curiosa por tecnologia, acredito que design e dados transformam ideias em experiências reais. Estou sempre buscando aprender e evoluir.</p>
 
             <address>
-                <p>Nova Friburgo, RJ</p>
-                <p>luanascosta19@gmail.com</p>
-                <p>membro desde...</p>
+                <p> Nova Friburgo, RJ </p>
+                <p> luanascosta19@gmail.com </p>
+                <p> Membro desde Setembro, 2025 </p>
             </address>
         
-        <ul>
-          <li>Educação</li>
-          <li>Meio Ambiente</li>
+        <ul className={styles.listaInteresses}>
+          <li>Saúde</li>
+          <li>Alimentação</li>
           <li>Assistência Social</li>
           <li>Design</li>
         </ul>
